1b406e51fe9fe11ff28c64838d8efc7f21ac44f8
CaioVSRodrigues <caiovsrodriguess@gmail.com>
2023-09-18 13:52:16 -0300

    Connection, CreateStatement, ExecuteQuery

--- jdbc1/src/app/Program.java
+++ jdbc1/src/app/Program.java
@@ -1,17 +1,38 @@
 package app;
 
 import db.DB;
+import db.DbException;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Program {
 
     public static void main(String[] args){
 
-        Connection conn = DB.getConnection();
+        Connection conn = null;
+        Statement st = null;
+        ResultSet rs = null;
 
+        try{
+            conn = DB.getConnection();
+            st = conn.createStatement();
+            rs = st.executeQuery("SELECT * FROM department");
 
+            while(rs.next()){
+                System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
 
-        DB.closeConnection();
+            }
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+        }
+        finally {
+            DB.closeResultSet(rs);
+            DB.closeStatement(st);
+            DB.closeConnection();
+        }
     }
 }
