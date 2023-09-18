d12599e3846a38c8099f149e72420c5bfc2ecb07
CaioVSRodrigues <caiovsrodriguess@gmail.com>
2023-09-18 14:36:36 -0300

    UPDATING salary

--- jdbc1/src/app/Program.java
+++ jdbc1/src/app/Program.java
@@ -17,6 +17,39 @@ public class Program {
 
 
         Connection conn = null;
+
+        PreparedStatement st = null;
+
+        try{
+            conn = DB.getConnection();
+
+            st = conn.prepareStatement(
+                    "UPDATE seller "
+                    + "SET BaseSalary = BaseSalary + ? "
+                    + "WHERE "
+                    + "(DepartmentId = ?)");
+
+            st.setDouble(1, 200.0);
+            st.setInt(2,2);
+
+            int rowsAffected = st.executeUpdate();
+
+            System.out.println("DONE! Rows affected: " + rowsAffected);
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+        }
+        finally {
+            DB.closeStatement(st);
+            DB.closeConnection();
+        }
+
+
+        /*
+
+
+
+        Connection conn = null;
         PreparedStatement st = null;
 
         try{
@@ -61,5 +94,7 @@ public class Program {
             DB.closeConnection();
 
         }
+
+        */
         }
 }
