89a256bd87f78f3a4d4bd026ef01a15ae9a868f9
CaioVSRodrigues <caiovsrodriguess@gmail.com>
2023-09-18 14:19:19 -0300

    Insert and show which is the id created

--- jdbc1/src/app/Program.java
+++ jdbc1/src/app/Program.java
@@ -3,9 +3,7 @@ package app;
 import db.DB;
 import db.DbException;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
@@ -29,7 +27,8 @@ public class Program {
                     "INSERT INTO seller"
                     + "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
                     +"VALUES "
-                    +"(?,?,?,?,?)");
+                    +"(?,?,?,?,?)",
+                    Statement.RETURN_GENERATED_KEYS);
 
             st.setString(1, "Carl Purple");
             st.setString(2, "carl@gmail.com");
@@ -39,7 +38,16 @@ public class Program {
 
             int rowsAffected = st.executeUpdate();
 
-            System.out.println("DONE! ROWS AFFECTED: " + rowsAffected);
+            if(rowsAffected > 0){
+                ResultSet rs = st.getGeneratedKeys();
+                while(rs.next()){
+                    int id = rs.getInt(1);
+                    System.out.println("Done! Id = " + id);
+                }
+            }
+            else{
+                System.out.println("nNo rows affected!");
+            }
 
         }
         catch(SQLException e) {
